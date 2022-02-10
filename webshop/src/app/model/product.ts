081d198b2fbc5c0affa4c9cbf15087655bb2af4f
iloth <loth.istvan@hotmail.com>
2022-02-10 23:33:03 +0100

    "Generic" table suggestion

--- webshop/src/app/model/product.ts
+++ webshop/src/app/model/product.ts
@@ -1,8 +1,9 @@
 export class Product {
+  [key: string] : any;
   id: number = 0;
   name: string = '';
   type: string = '';
-  catId: number = 0;
+  catID: number = 0;
   description: string = '';
   price: number = 0;
   featured: boolean = false;
