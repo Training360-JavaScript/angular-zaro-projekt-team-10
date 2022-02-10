931e2cff0a7084caed85855acfc31cfb66c665f8
DiSLucky7 <nagyklevi@gmail.com>
2022-02-10 22:18:23 +0100

    generic-table unsolved error

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GenericTableModule } from '@angular-generic-table/core';
+// import { GenericTableModule } from 'angular-generic-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,8 @@ import { HeaderComponent } from './common/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GenericTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
