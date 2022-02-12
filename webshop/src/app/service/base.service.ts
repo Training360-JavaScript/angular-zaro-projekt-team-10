90138da0c43acf5b8d770e0ad0b638fe7fe0b1fa
iloth <loth.istvan@hotmail.com>
2022-02-12 07:46:33 +0100

    BAseService: complete functionality

--- webshop/src/app/service/base.service.ts
+++ webshop/src/app/service/base.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-export abstract class BaseService<Model> {
+export abstract class BaseService<Model extends { id: number }> {
   constructor(
     private httpClient: HttpClient,
     protected readonly entityUrl: string
@@ -20,4 +20,21 @@ export abstract class BaseService<Model> {
     const url = `${this.url}/${id}`;
     return this.httpClient.get<Model>(url);
   }
+
+  add(item: Model): Observable<Model> {
+    const url = `${this.url}`;
+    item.id = 0;
+    return this.httpClient.post<Model>(url, item);
+  }
+
+  update(item: Model): Observable<Model> {
+    const url = `${this.url}/${item.id}`;
+    return this.httpClient.put<Model>(url, item);
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.url}/${id}`;
+    return this.httpClient.delete(url);
+  }
+
 }
--- webshop/src/environments/environment.ts
+++ webshop/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'https://nettuts.hu/jms/js4-002/'
+  backendUrl: 'http://localhost:3000/', //https://nettuts.hu/jms/team10/'
 };
 
 /*
