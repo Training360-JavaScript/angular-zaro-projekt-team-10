a8db0098315918a2b7d313effc0aef933060e826
iloth <loth.istvan@hotmail.com>
2022-02-20 19:17:26 +0100

    JOINED TABLES BUGFIX

--- webshop/src/app/service/order-line.service.ts
+++ webshop/src/app/service/order-line.service.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@angular/core';
 import { forkJoin, map, Observable } from 'rxjs';
 import { OrderLine, OrderLineDisplay } from '../model/order-line';
 import { BaseService } from './base.service';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderLineService extends BaseService<OrderLine> {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService
+  ) {
     super(http, 'order-line');
   }
 
@@ -16,5 +20,31 @@ export class OrderLineService extends BaseService<OrderLine> {
     return this.getAll().pipe(
       map((lines) => lines.filter((line) => line.orderID === orderId))
     )
+  }
+
+  getAllDisplayByOrderId(orderId: number): Observable<OrderLineDisplay[]> {
+    const join$ = forkJoin({
+      lines: this.getAllByOrderId(orderId),
+      products: this.productService.getAll()
+    });
+
+    return join$.pipe(
+      map((result) => {
+        return result.lines.map((line) => {
+          const product = result.products.find((product) => product.id === line.productID);
+
+          return new OrderLineDisplay(
+            line.id,
+            line.orderID,
+            line.productID,
+            product?.name,
+            line.amount,
+            product?.price,
+            line.amount * (product?.price ?? 0)
+          );
+        })
+      })
+    );
+
   }
 }
